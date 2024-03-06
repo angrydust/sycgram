88bf5a957d9ce0e4b4da2ebf0c6a6e7b424b45a1
Chan ai <angrydust0409@gmail.com>
2024-03-06 14:56:08 +0800

    update da plugin error

--- plugins/da.py
+++ plugins/da.py
@@ -1,42 +1,43 @@
 from asyncio import sleep
-from pyrogram import Client, filters
+
+from pyrogram import Client
 from pyrogram.types import Message
 
 
 @Client.on_message(command=('da'))
-async def da(context):
-    if len(context.parameter) > 2 or len(context.parameter) == 0:
-        await context.edit("\n呜呜呜，请执行 `-da true` 来删除所有消息。")
+async def da(cli: Client, msg: Message):
+    if len(msg.parameter) > 2 or len(msg.parameter) == 0:
+        await msg.edit_text("\n呜呜呜，请执行 `-da true` 来删除所有消息。")
         return
-    if context.parameter[0] != "true":
-        await context.edit("\n呜呜呜，请执行 `-da true` 来删除所有消息。")
+    if msg.parameter[0] != "true":
+        await msg.edit_text("\n呜呜呜，请执行 `-da true` 来删除所有消息。")
         return
-    await context.edit('正在删除所有消息 . . .')
-    input_chat = await context.get_input_chat()
+    await msg.edit_text('正在删除所有消息 . . .')
+    input_chat = await msg.chat.id
     messages = []
     count = 0
-    async for message in context.client.iter_messages(input_chat, min_id=1):
-        messages.append(message)
+    async for messageid in msg.client.iter_messages(input_chat, min_id=1):
+        messages.append(messageid)
         count += 1
         messages.append(1)
         if len(messages) == 100:
-            await context.client.delete_messages(input_chat, messages)
+            await cli.delete_messages(input_chat, messages)
             messages = []
 
     if messages:
-        await context.client.delete_messages(input_chat, messages)
-    await log(f"批量删除了 {str(count)} 条消息。")
+        await cli.delete_messages(input_chat, messages)
+    await msg.reply_text(f"批量删除了 {str(count)} 条消息。")
     try:
-        notification = await send_prune_notify(context, count)
+        notification = await send_prune_notify(msg, count)
     except:
         return
     await sleep(.5)
     await notification.delete()
 
 
-async def send_prune_notify(context, count):
-    return await context.client.send_message(
-        context.chat_id,
+async def send_prune_notify(msg, count):
+    return await msg.reply_text(
+        msg.chat.id,
         "批量删除了 "
         + str(count)
         + " 条消息。"
